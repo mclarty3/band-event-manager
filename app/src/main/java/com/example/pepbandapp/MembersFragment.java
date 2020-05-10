21768f264584ef3c64dcec7b877f094c99fc6e4f
ChiaraG29 <chiarargiammatteo@gmail.com>
2020-05-10 16:21:40 -0400

    Added string to date conversion method in ScheduleFragment Added csv read-in ability to Members Fixed xml errors with onClick functions

--- app/src/main/java/com/example/pepbandapp/MembersFragment.java
+++ app/src/main/java/com/example/pepbandapp/MembersFragment.java
@@ -1,10 +1,17 @@
 package com.example.pepbandapp;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 
 public class MembersFragment extends AppCompatActivity {
     TextView studentNameTextView, yearTextView, instrumentTextView, emailTextView, membersTextView;
@@ -40,5 +47,35 @@ public class MembersFragment extends AppCompatActivity {
     public void groupByInstrument(View view) {
         String resultsString = dbHandler.groupByInstrument();
         membersTextView.setText(resultsString);
+    }
+
+    public void readInMembers(View view) {
+        InputStream is = getResources().openRawResource(R.raw.members_data);
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(is, Charset.forName("UTF-8"))
+        );
+
+        String line = "";
+        while (true) {
+            try {
+                //split by ","
+                String[] tokens = line.split(",");
+
+                //read the data
+                Member member = null;
+                member.set_name(tokens[0]);
+                member.set_year(tokens[1]);
+                member.set_instrument(tokens[2]);
+                member.set_email(tokens[3]);
+                dbHandler.addMember(member);
+
+                Log.d("ScheduleActivity", "Just created: " + member);
+
+                if (!((line = reader.readLine()) != null)) break;
+            } catch (IOException e) {
+                Log.wtf("ScheduleActivity", "Error reading data file on line " + line, e);
+                e.printStackTrace();
+            }
+        }
     }
 }
--- app/src/main/java/com/example/pepbandapp/ScheduleFragment.java
+++ app/src/main/java/com/example/pepbandapp/ScheduleFragment.java
@@ -2,6 +2,7 @@ package com.example.pepbandapp;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -12,8 +13,11 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Date;
 
 public class ScheduleFragment extends AppCompatActivity{
 
@@ -42,7 +46,9 @@ public class ScheduleFragment extends AppCompatActivity{
         scheduleTextView.setText(dbString);
     }
 
-    public void readInData() {
+    SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+
+    public void readInData(View view) {
         InputStream is = getResources().openRawResource(R.raw.events_data);
         BufferedReader reader = new BufferedReader(
                 new InputStreamReader(is, Charset.forName("UTF-8"))
@@ -59,13 +65,14 @@ public class ScheduleFragment extends AppCompatActivity{
                 event.set_name(tokens[0]);
                 event.set_info(tokens[1]);
                 event.set_location(tokens[2]);
-                event.set_date(tokens[3]); //need to convert string to date somehow
+                Date date = formatter.parse(tokens[3]);
+                event.set_date(date);
                 dbHandler.addEvent(event);
 
                 Log.d("ScheduleActivity", "Just created: " + event);
 
                 if (!((line = reader.readLine()) != null)) break;
-            } catch (IOException e) {
+            } catch (IOException | ParseException e) {
                 Log.wtf("ScheduleActivity", "Error reading data file on line " + line, e);
                 e.printStackTrace();
             }
